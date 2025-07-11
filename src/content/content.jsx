cbf06ff21a13ebf83652610d4280517a7c8763f1
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-11 08:12:58 -0300

    fix: Diminui o tamanho da fonte na Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -135,7 +135,7 @@ const content = {
         </h1>
       ),
       subtitle: (
-        <p>
+        <p className="text-paragraph3">
           * Pós graduada em Processo Civil e Direito Civil pela Escola Paulista
           de Direito - EPD
           <br />* Pós graduada em Direito Processual do Trabalho e Direito do

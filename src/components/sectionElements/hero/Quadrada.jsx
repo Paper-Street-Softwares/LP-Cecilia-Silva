20a73479b5b93758d54607542104b1a18882fbdd
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-11 11:23:52 -0300

    fix: Shadow na hero

--- src/components/sectionElements/hero/Quadrada.jsx
+++ src/components/sectionElements/hero/Quadrada.jsx
@@ -124,7 +124,7 @@ export default function Quadrada({ appDownloadButtons, colorMode }) {
             <div className="flex justify-center w-full tablet1:w-[450px] desktop1:w-[42%] desktop2:w-[42.8%]">
               <MotionDivDownToUp className="relative flex justify-center w-full">
                 <div
-                  className="bg-top mt-[20px] desktop1:mt-0 rounded-xl w-full phone1:h-[300px] phone1:max-w-[300px] phone2:h-[355px] phone2:max-w-[355px] phone3:h-[405px] phone3:max-w-[405px] tablet1:h-[576px] tablet1:min-w-[576px] tablet2:h-[690px] tablet2:min-w-[690px] desktop1:h-[400px] desktop1:min-w-[200px] desktop2:h-[400px] desktop2:min-w-[400px] bg-cover bg-no-repeat shadow-custom-opacity shadow-shadowAbouts/30"
+                  className="bg-top mt-[20px] desktop1:mt-0 rounded-xl w-full phone1:h-[300px] phone1:max-w-[300px] phone2:h-[355px] phone2:max-w-[355px] phone3:h-[405px] phone3:max-w-[405px] tablet1:h-[576px] tablet1:min-w-[576px] tablet2:h-[690px] tablet2:min-w-[690px] desktop1:h-[400px] desktop1:min-w-[200px] desktop2:h-[400px] desktop2:min-w-[400px] bg-cover bg-no-repeat desktop1:shadow-custom-opacity desktop1:shadow-shadowAbouts/30"
                   style={{
                     backgroundImage: `url(${content.texts.hero.heroDefaultImage})`,
                   }}

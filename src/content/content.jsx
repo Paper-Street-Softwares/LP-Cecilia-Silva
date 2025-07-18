2b6ed0e71fb17e050a2f5c78c53a1ebb7d3d5bff
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-18 09:02:15 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -130,12 +130,12 @@ const content = {
           <br />
           OAB/SP 312.967
           <br />
-          <br />
-          Advogada, atuante desde 2011.
         </h1>
       ),
       subtitle: (
         <p className="text-paragraph1">
+          Advogada, atuante desde 2011.
+          <br />
           * Pós graduada em Processo Civil e Direito Civil pela Escola Paulista
           de Direito - EPD
           <br />* Pós graduada em Direito Processual do Trabalho e Direito do

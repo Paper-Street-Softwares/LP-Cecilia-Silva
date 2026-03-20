efdee08fbf05eb387f38131c23ed02d4d5dd3db8
Edison Matos <edison.matos@live.com>
2026-03-20 17:52:12 -0300

    feat: Ajuste de shadows

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -60,7 +60,7 @@ export default function About({
           <img
             src={content.texts.about.imagem.img}
             alt={content.texts.about.imagem.alt}
-            className="w-[100%] desktop1:w-[415px] desktop2:w-[485px] rounded-xl shadow-custom-opacity shadow-shadowAbouts/30"
+            className="w-[100%] desktop1:w-[415px] desktop2:w-[485px] rounded-xl shadow-custom-opacity shadow-shadowAbouts/10"
           />
         </MotionDivDownToUp>
         {/* <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
